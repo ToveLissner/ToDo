5d24b7056fb9c3965562f3ac5dd772ba5b6c43ca
ToveLissner <tove_lissner@hotmail.com>
2022-11-08 13:02:29 +0100

    Funktioner och lite CSS

--- src/js/main.js
+++ src/js/main.js
@@ -1,102 +1,129 @@
 import { Todo } from "./models/Todo";
 
-// FUNKTIONER //
+let todos = [];
 
-function close() {
-  let parent = this.parentElement;
-  for (let i = 0; i < close.length; i++) {
-    parent.style.display = "none";
-  }
-}
-
-// RESTEN //
-
-let todos = [
-  new Todo("Springa", false),
-  new Todo("Gå", false),
-  new Todo("Skola", false),
-];
+let form = document.getElementById("form");
+let userInput = document.getElementById("userInput");
 
 let ul = document.getElementById("theList");
-let liTag = document.getElementsByTagName("LI");
 
-// LOOPAR - in i funktioner??? //
+// FUNKTIONER //
 
-for (let i = 0; i < todos.length; i++) {
-  ul.innerHTML += "<li>" + todos[i].what + "</li>";
-  let span = document.createElement("SPAN");
-  let txt = document.createTextNode("\u00D7");
-  span.className = "theList__li__span";
-  span.appendChild(txt);
-  liTag[i].appendChild(span);
-}
+function createHTMLHeader() {}
 
-let spanClose = document.getElementsByClassName("theList__li__span");
-
-spanClose.addEventListener("click", close);
-
-let div = document.getElementById("div");
-
-localStorage.setItem("todos", JSON.stringify(todos));
-
-let fromLS = localStorage.getItem("todos");
-let todosObj = JSON.parse(fromLS);
-
-todos = todosObj.map((todo) => {
-  return new Todo(todo.what);
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  addTodo(userInput.value);
 });
 
-console.log(todos);
+function addTodo(userInput) {
+  let newTodos = new Todo(userInput, false);
+  todos.push(newTodos);
 
-function myFunction(myList) {
-  for (let i = 0; i < myList.length; i++) {
-    let container = document.createElement("div");
-    let heading1 = document.createElement("h3");
-    button1 = document.createElement("button");
+  saveToLS(todos);
+}
 
-    //let pTag1 = document.createElement("p");
-
-    container.classList = "container";
-    heading1.classList = "container__what";
-    button1.classList = "container__button";
-    //pTag1.classList = "container__where";
-    heading1.innerHTML += myList[i].what;
-    button1.innerHTML += "klar";
-    //pTag1.innerHTML += myList[i].where;
-
-    container.appendChild(heading1);
-    container.appendChild(button1);
-    //container.appendChild(pTag1);
-
-    div.appendChild(container);
+function createHTML(todos) {
+  ul.innerHTML = "";
+  for (let i = 0; i < todos.length; i++) {
+    ul.innerHTML += "<li>" + todos[i].title + "</li>";
   }
 }
 
-myFunction(todos);
+function saveToLS(todos) {
+  localStorage.setItem("todos", JSON.stringify(todos));
 
-let inputElement = document.getElementById("userInput");
+  createHTML(todos);
+}
 
-let theText = inputElement.value;
+function getFromLS() {
+  let fromLS = localStorage.getItem("todos");
+  if (fromLS) {
+    todos = JSON.parse(fromLS);
+    createHTML(todos);
+  }
+}
 
-console.log(theText);
+getFromLS();
 
-// let inputNew = document.getElementById("inputNew");
+function todoCompleted() {}
 
-// inputNew.addEventListener("click", addNew);
+function deleteTodo() {}
 
-//function addNew(todo) {
-//todos.push(todo);
-//myFunction(todo);
-//
+// todos = todosObj.map((todo) => {
+//   return new Todo(todo.title, todo.done);
+// });
 
-// till html <div id="newTask">
-// <input type="text" id="userInput" value="">
-// <button type="button" id="buttonAdd">Lägg till</button> </div>
+// let liTag = document.getElementsByTagName("LI");
 
-// let addButton = document.getElementById("buttonAdd");
+// // LOOPAR - in i funktioner??? //
 
-// function addClick() {
-// console.log("klick hände");
+// for (let i = 0; i < todos.length; i++) {
+//   ul.innerHTML += "<li>" + todos[i].what + "</li>";
+//   let span = document.createElement("SPAN");
+//   let txt = document.createTextNode("\u00D7");
+//   span.className = "theList__li__span";
+//   span.appendChild(txt);
+//   liTag[i].appendChild(span);
 // }
 
-// addButton.addEventListener("click", addClick);
+// let spanClose = document.getElementsByClassName("theList__li__span");
+
+// spanClose.addEventListener("click", close);
+
+// let div = document.getElementById("div");
+
+// console.log(todos);
+
+// function myFunction(myList) {
+//   for (let i = 0; i < myList.length; i++) {
+//     let container = document.createElement("div");
+//     let heading1 = document.createElement("h3");
+//     button1 = document.createElement("button");
+
+//     //let pTag1 = document.createElement("p");
+
+//     container.classList = "container";
+//     heading1.classList = "container__what";
+//     button1.classList = "container__button";
+//     //pTag1.classList = "container__where";
+//     heading1.innerHTML += myList[i].what;
+//     button1.innerHTML += "klar";
+//     //pTag1.innerHTML += myList[i].where;
+
+//     container.appendChild(heading1);
+//     container.appendChild(button1);
+//     //container.appendChild(pTag1);
+
+//     div.appendChild(container);
+//   }
+// }
+
+// myFunction(todos);
+
+// let inputElement = document.getElementById("userInput");
+
+// let theText = inputElement.value;
+
+// console.log(theText);
+
+// // let inputNew = document.getElementById("inputNew");
+
+// // inputNew.addEventListener("click", addNew);
+
+// //function addNew(todo) {
+// //todos.push(todo);
+// //myFunction(todo);
+// //
+
+// // till html <div id="newTask">
+// // <input type="text" id="userInput" value="">
+// // <button type="button" id="buttonAdd">Lägg till</button> </div>
+
+// // let addButton = document.getElementById("buttonAdd");
+
+// // function addClick() {
+// // console.log("klick hände");
+// // }
+
+// // addButton.addEventListener("click", addClick);
